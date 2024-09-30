78aab551a0aa79ed4a3d12bc0dd303a65e5981b9
Birkin James Diana <birkin.diana@gmail.com>
2024-09-30 14:07:39 -0400

    adds tracker and get-mods functions.

--- update_org_mods.py
+++ update_org_mods.py
@@ -1,7 +1,10 @@
-import logging, pathlib
+import json, logging, pathlib
+import httpx
 
 
 log = logging.getLogger( __name__ )
+
+MODS_URL_PATTERN = 'https://repository.library.brown.edu/storage/{PID}/MODS/'
 
 
 ## helper functions -------------------------------------------------
@@ -21,6 +24,41 @@ def load_pids( pid_full_fpath: pathlib.Path ) -> list:
     return pids
 
 
+def load_tracker( pid_full_fpath: pathlib.Path ) -> dict:
+    """
+    Load tracker.
+    Assumes tracker is in same directory as pid-file.
+    """
+    tracker_full_fpath = pid_full_fpath.parent.joinpath( 'tracker.json' )
+    tracker = {}
+    if tracker_full_fpath.exists():
+        with open( tracker_full_fpath, 'r' ) as f:
+            tracker = json.loads( f.read() )
+    log.debug( f'tracker, ```{tracker}```' )
+    return tracker
+
+
+def check_if_pid_was_processed( pid: str, tracker: dict ) -> str:
+    """
+    Check if pid was processed.
+    """
+    status = tracker.get( pid, 'not_done' )
+    log.debug( f'pid, ``{pid}``; status, ``{status}``' )
+    return status
+
+
+def get_mods( pid: str ) -> str:
+    """
+    Get mods.
+    """
+    mods_url = MODS_URL_PATTERN.format( PID=pid )
+    log.debug( f'mods_url, ```{mods_url}```' )
+    mods_utf8 = httpx.get( mods_url )
+    mods = mods_utf8.decode( 'utf-8' )
+    assert type(mods) == str
+    return mods
+
+
 ## manager function -------------------------------------------------
 
 
@@ -31,11 +69,17 @@ def manage_update( pid_full_fpath: pathlib.Path ):
     """
     ## get list of pids from file -----------------------------------
     pids: list = load_pids( pid_full_fpath )
+    assert len( pids ) == 97
+    ## load tracker -------------------------------------------------
+    tracker = load_tracker( pid_full_fpath )
     ## loop over pids -----------------------------------------------
     for pid in pids:
-        assert type(pid) == int
+        assert type(pid) == str
+        ## check if pid has been processed --------------------------
+        if check_if_pid_was_processed( pid, tracker ) == 'done':
+            continue
         ## get mods -------------------------------------------------
-        mods = get_mods( pid )
+        mods: str = get_mods( pid )
         ## update mods ----------------------------------------------
         updated_mods = update_mods( mods )
         ## save mods ------------------------------------------------
