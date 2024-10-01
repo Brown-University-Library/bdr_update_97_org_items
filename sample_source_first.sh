a86260d0f2a9291ec86c354232ef5b1cc992037e
Birkin James Diana <birkin.diana@gmail.com>
2024-10-01 13:11:21 -0400

    adds correct-run-directory shell-script.

--- sample_source_first.sh
+++ sample_source_first.sh
@@ -1,4 +1,16 @@
-## USAGE: run this script from the `bdr_update_97_org_items` directory
+#!/bin/bash
+
+## USAGE: run this script from the outer "stuff" directory
+
+
+## check current directory -----------------------------------------------
+current_dir=$(basename "$PWD")
+if [[ "$current_dir" == *stuff || "$current_dir" == *outer ]]; then
+    echo "Running script in a valid directory."
+else
+    echo "Error: This script must be run from a directory ending in 'stuff' or 'outer'."
+    exit 1
+fi
 
 
 ## `u97__` envars are used by this `bdr_update_97_org_items` code --------
@@ -8,8 +20,8 @@ export U97__MODS_URL_PATTERN="url-to/storage/{PID}/MODS/"  # should match server
 
 ## the `UM__` envars are used by the `update_mods_py_binary` -------------
 export UM__API_AGENT="update_mods_script.py"
-export UM__API_IDENTITY="THE:SHIB:IDENTITY"  # the shib-identity
-export UM__API_ROOT_URL="api/private/url"  # the root api-url, used to build the full api-url
+export UM__API_IDENTITY="THE:SHIB:IDENTITY"     # the shib-identity
+export UM__API_ROOT_URL="api/private/url"       # the root api-url, used to build the full api-url
 export UM__LOGLEVEL="DEBUG"
 export UM__MESSAGE="adds indicator to MODS that this is an organization-item"
 
